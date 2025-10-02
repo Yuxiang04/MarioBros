484d811ec8969f8d41e73b0694d9532fed984432
Yuxiang <yuxweng@ucm.es>
2025-10-02 10:28:05 +0200

    game objects

--- src/tp1/logic/GameObjectContainer.java
+++ src/tp1/logic/GameObjectContainer.java
@@ -1,10 +1,22 @@
 package tp1.logic;
 
+import java.util.List;
+
+import tp1.logic.gameobjects.ExitDoor;
+import tp1.logic.gameobjects.Goomba;
+import tp1.logic.gameobjects.Land;
+import tp1.logic.gameobjects.Mario;
+
 public class GameObjectContainer {
 	//TODO fill your code
 
-	/*private List<Land> landList;
+	private List<Land> landList;
 	private List<Goomba> goombaList;
 	private ExitDoor exit;
-	private Mario mario;*/
+	private Mario mario;
+	
+	
+	public void add(Land land) {
+		
+	}
 }
---
+++ src/tp1/logic/gameobjects/Land.java
@@ -0,0 +1,14 @@
+package tp1.logic.gameobjects;
+
+import tp1.logic.Position;
+import tp1.view.Messages;
+
+public class Land {
+
+	private Position pos;
+	
+	public String getIcon() {
+		return Messages.LAND;
+		
+	}
+}
